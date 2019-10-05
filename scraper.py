d6223fb1cfe8cce702d55100112d56cc73c3c37f
Segilo <Segilo@users.noreply.github.com>
2019-10-05 09:47:12 +0900

    スクレイピングするサイトを変更するよう変更
    ついでにtypo修正

--- scraper.py
+++ scraper.py
@@ -12,18 +12,23 @@ class Scraper:
         sp = BeautifulSoup(html, parser)
         for tag in sp.find_all("a"):
             url = tag.get("href")
-            # "htm"を含むURLがないみたいで
-            # 本文のとおり↓では何も表示されなかった
-            # if url is None:
-            #    continue
-            # if "html" in url:
-            #    prrint("\n" + url)
-            # 仕方がないので、hrefタグを全部出してみる
-            print(url)
+            if url is None:
+               continue
+            if "html" in url:
+               print("\n" + url)
+            # 上記がうまくいかないときに、hrefタグを全部出してみる版↓
+            # print(url)
             # thanks! https://teratail.com/questions/150749
+            
             # ちなみに日経BP社から2018年10月16日付で補足↓も出ているが
             # https://shop.nikkeibp.co.jp/front/commodity/0000/C92270/
             # こちらも構造が変更になっているようで、うまくいかなかった
 
-news = "https://news.google.com/"
+# google news のサイトでは
+# "html"を含むURLがないみたいで
+# 本文のとおり↓では何も表示されなかった 
+# news = "https://news.google.com/"
+news = "https://www.yahoo.com/"
+# 原著者のGitHubページでは、↑のようにサイトを変更するのが簡単、ということになってる様子
+# https://github.com/calthoff/self_taught/issues/21#issuecomment-457426380
 Scraper(news).scrape()
